7f6b8e6fa61f9c862b0c4fb6bcd82eb81d584c1c
chene <b.jean.ch@gmail.com>
2019-01-14 23:24:48 +0100

    add test python application

---
+++ app_python/app/lambda_function.py
@@ -0,0 +1,22 @@
+import boto3
+import os
+import random
+
+def handler():
+    # table_name = os.environ['DYNAMODB_TABLE']
+    table_name = "store_last_values"
+    dynamodb = boto3.resource('dynamodb')
+    id_to_store = "id_" + str(random.randint(1,10000))
+    var_to_store = "var_" + str(random.randint(1,10000))
+    table = dynamodb.Table(table_name)
+    table.put_item(
+       Item={
+            'id': id_to_store,
+            'var': var_to_store,
+            'time': "23456789",
+            'value': "89"
+        }
+    )
+
+if __name__ == "__main__":
+    handler()
