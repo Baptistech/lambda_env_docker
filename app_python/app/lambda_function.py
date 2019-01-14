20054dfeefbae656548e74f6b5cc6ca4c566763b
chene <b.jean.ch@gmail.com>
2019-01-15 00:18:54 +0100

    update dependancies and code

--- app_python/app/lambda_function.py
+++ app_python/app/lambda_function.py
@@ -5,6 +5,7 @@ import random
 def handler():
     # table_name = os.environ['DYNAMODB_TABLE']
     table_name = "store_last_values"
+    client = boto3.client("client", region_name='us-west-2')
     dynamodb = boto3.resource('dynamodb')
     id_to_store = "id_" + str(random.randint(1,10000))
     var_to_store = "var_" + str(random.randint(1,10000))
